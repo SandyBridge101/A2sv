685d2c936d8228d13a2ea0ca58d557b826c8e83f
Tariq Nasser Deen <99515683+SandyBridge101@users.noreply.github.com>
2023-11-02 21:55:48 +0000

    Create cookies.py

---
+++ programming solutions 2/cookies.py
@@ -0,0 +1,28 @@
+class Solution:
+    def distributeCookies(self, cookies: List[int], k: int) -> int:
+        state = [0]*k
+        ans = sum(cookies)
+        
+        def search(state, idx):
+            nonlocal ans
+            if idx == k:
+                if sum(cookies) == 0:
+                    ans = min(ans, max(state))
+                return
+            
+            for i in range(len(cookies)):
+                if cookies[i] == 0:
+                    continue
+                state[idx] += cookies[i]
+                temp = cookies[i]
+                cookies[i] = 0
+
+                if state[idx] < ans:
+                    search(state, idx)
+                    search(state, idx+1)
+
+                state[idx] -= temp
+                cookies[i] = temp
+
+        search(state, 0)
+        return ans
