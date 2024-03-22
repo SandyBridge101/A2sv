6ee827440014cee5735a1ff56bba7e4164af08fa
Tariq Nasser Deen <99515683+SandyBridge101@users.noreply.github.com>
2024-03-22 10:28:54 +0000

    Added solution for Find Greatest Common Divisor of Array

---
+++ programming solutions 2/find-greatest-common-divisor-of-array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findGCD(self, nums: List[int]) -> int:
+
+        def gcd(a,b):
+            if not b:
+                return a
+            return gcd(b,a%b)
+
+        return gcd(min(nums),max(nums))
+
+        
